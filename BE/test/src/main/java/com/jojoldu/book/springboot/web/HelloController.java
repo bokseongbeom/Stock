3a2ddea37f44898364074acb8a1ba6f3252dc2e6
bok <vvs9439@naver.com>
2022-12-13 12:55:52 +0900

    dtoController 작성

--- BE/test/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ BE/test/src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,9 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- BE/test/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ BE/test/src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -31,4 +31,18 @@ public class HelloControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                        get("/hello/dto")
+                                .param("name", name)
+                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
