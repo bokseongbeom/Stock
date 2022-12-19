258c7619bfa4cd11014bd3f6f342aeafc0df2ee9
bok <vvs9439@naver.com>
2022-12-19 22:13:52 +0900

    BE study

---
+++ BE/test/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+}
