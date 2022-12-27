ee1fee85f31e7b72d5b4cfd04fe6b87073d478ed
bok <boksb1@naver.com>
2022-12-27 23:09:23 +0900

    사랑

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,24 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
