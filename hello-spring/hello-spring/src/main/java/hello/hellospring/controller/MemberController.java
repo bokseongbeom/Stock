5c85702689fa9f68fe3b9bf80e95278d292fc6a6
bok <boksb1@naver.com>
2022-12-30 21:02:51 +0900

    사라

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+}
--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
