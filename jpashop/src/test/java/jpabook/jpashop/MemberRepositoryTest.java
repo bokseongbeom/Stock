de7d17d76a2cf9c43546b7c5e07ed68f19bb90a6
bok <boksb1@naver.com>
2023-01-03 00:12:35 +0900

    ff

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@RunWidth
+@SpringBootTest
+public class MemberRepositoryTest {
+
+}
