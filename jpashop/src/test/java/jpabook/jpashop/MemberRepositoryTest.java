d77f8d23fbc381ad5df7746e20d2088ebc9a0fd3
bok <boksb1@naver.com>
2023-01-08 22:20:36 +0900

    fe

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,15 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.*;
 
 @SpringBootTest
 public class MemberRepositoryTest {
 
         @Autowired MemberRepository memberRepository;
         @Test
+        @Transactional
         public void testMember() throws  Exception {
                 //given
                 Member member = new Member();
@@ -25,6 +25,6 @@ public class MemberRepositoryTest {
                 //then
                 Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
                 Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-
+                Assertions.assertThat(findMember).isEqualTo(member);
         }
 }
